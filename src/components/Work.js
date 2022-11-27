2c31b27768738fee202803886c886d11e88da82f
HarshRanjan28 <64592351+HarshRanjan28@users.noreply.github.com>
2022-11-27 16:23:23 +0530

    Update Work.js

---
+++ src/components/Work.js
@@ -0,0 +1,25 @@
+import React from "react";
+import WorkCradData from "../components/WorkCradData";
+import WorkCard from "../components/WorkCard";
+const Work = () => {
+  return (
+    <>
+      <div className="work-container">
+        <h1 className="project-heading">Projects</h1>
+        <div className="project-container">
+          {WorkCradData.map((val, ind) => {
+            return (
+              <WorkCard
+                key={ind}
+                imgsrc={val.imgsrc}
+                title={val.title}
+                view={val.View}
+                source={val.Source}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
